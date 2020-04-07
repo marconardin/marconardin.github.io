acb862695ee9c4289ca6cfe53818e03ff92a6294
Marco Nardin <27.marco@gmail.com>
2020-04-06 23:23:45 -0400

    adding running related material to website

---
+++ pace_calculator.js
@@ -0,0 +1,23 @@
+function get_distance_type()
+{
+    return document.querySelector('input[name=measurement]:checked').value;
+}
+
+function convert_decimal_to_seconds(pace)
+{
+    return Math.round((((pace*100) % 100) * .6) * 100 / 100);
+}
+
+function calculate_pace()
+{
+    let distance_type = get_distance_type();
+    let distance = document.querySelector("#distance").value;
+    let time = document.querySelector("#time").value;
+
+    // calculate pace and convert decimal to seconds
+    let pace = time / distance;
+    let seconds = convert_decimal_to_seconds(pace);
+    pace = Math.floor(pace);
+
+    document.querySelector("#calculated_pace").innerHTML = "Your pace: " + pace + " minutes and " + seconds + " seconds per " + distance_type;
+}
